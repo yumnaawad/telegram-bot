532cb784ac0c9a30ec3a9a5bc971253a23b369e2
yumnaawad <155652796+yumnaawad@users.noreply.github.com>
2025-09-07 03:42:13 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ nest_asyncio.apply()
 from telegram import InputFile
 
 
-TOKEN = "8115750679:AA3bVGEGXRICAPPL1z2UXKsfH9xvEwIPjo"
+TOKEN = "8115750679:AAF3bVGEGXRICAPPL1z2UXKsfH9xvEwIPjo"
 LOGIN = 1
 SESSIONS_FILE = "sessions.json"
 
