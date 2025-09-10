2978534e28335ca4d4870e86678864114446f433
yumnaawad <155652796+yumnaawad@users.noreply.github.com>
2025-09-10 07:29:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ btn8 = types.InlineKeyboardButton("📸 الصور", callback_data="photo")
 
     # إضافتهم للوحة (row_width=2 = صفين، كل صف فيه زرين)
 keyboard.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8)
-reply_markup = InlineKeyboardMarkup(keyboard)
+reply_markup = keyboard
 
     bot.send_message(
         message.chat.id,
